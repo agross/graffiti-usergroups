d4161b15b34bf82c576e07d4e785baa8e6c724f6
agross <agross@170fb600-b34b-0410-ab5d-8154dd4c2d08>
2008-03-31 16:26:59 +0000

    Labeled automatically by TeamCity

---
+++ Projects/.NET User Group Leipzig/tags/DnugLeipzig-1.0.20.532/source/app/DnugLeipzig.Extensions/Repositories/IRepository.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+using DnugLeipzig.Extensions.Filters;
+
+using Graffiti.Core;
+
+namespace DnugLeipzig.Extensions.Repositories
+{
+	public interface IRepository<T> where T : class
+	{
+		Data Data
+		{
+			get; 
+		}
+
+		List<T> Get(params IPostFilter[] filters);
+
+		T Get(int id);
+
+		Category GetCategory();
+	}
+}
---
+++ Projects/.NET User Group Leipzig/tags/DnugLeipzig-1.0.20.532/source/app/DnugLeipzig.Extensions/Repositories/PostRepository.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+
+using DnugLeipzig.Extensions.Filters;
+
+using Graffiti.Core;
+
+namespace DnugLeipzig.Extensions.Repositories
+{
+	public abstract class PostRepository:IRepository<Post>
+	{
+		protected readonly string _categoryName;
+		protected static readonly Data Data = new Data();
+
+		public PostRepository(string categoryName)
+		{
+			if (String.IsNullOrEmpty(categoryName))
+			{
+				throw new ArgumentOutOfRangeException("categoryName");
+			}
+
+			_categoryName = categoryName;
+		}
+
+		#region IRepository<Post> Members
+		protected PostCollection PostsByCategoryDisableHomepageOverride(int count)
+		{
+			// HACK
+			// Temporarily disable homepage overrides to get all posts of the category, even if they aren't
+			// displayed on the home page. This is useful for general "overview" teasers that show all content
+			// independent of the page.
+			bool useCustomHomeList = Data.Site.UseCustomHomeList;
+			Data.Site.UseCustomHomeList = false;
+			PostCollection posts = Data.PostsByCategory(_categoryName, count);
+			Data.Site.UseCustomHomeList = useCustomHomeList;
+
+			return posts;
+		}
+
+		Data IRepository<Post>.Data
+		{
+			get { return Data; }
+		}
+
+		public abstract List<Post> Get(params IPostFilter[] filters);
+
+		public virtual Post Get(int id)
+		{
+			return Data.GetPost(id);
+		}
+
+		public virtual Category GetCategory()
+		{
+			return Data.GetCategory(_categoryName);
+		}
+		#endregion
+	}
+}
