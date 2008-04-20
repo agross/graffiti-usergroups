0fbfdea317b3eb0a3152a193f350c53ea3277149
agross <agross@170fb600-b34b-0410-ab5d-8154dd4c2d08>
2008-04-20 03:02:42 +0000

    Fixing broken file references

---
+++ Projects/.NET User Group Leipzig/trunk/source/app/DnugLeipzig.Definitions/Configuration/IEventPluginConfigurationSource.cs
@@ -0,0 +1,45 @@
+namespace DnugLeipzig.Definitions.Configuration
+{
+	public interface IEventPluginConfigurationSource : IPluginConfigurationSource
+	{
+		string DateFormat
+		{
+			get;
+		}
+
+		string EndDateField
+		{
+			get;
+		}
+
+		string LocationField
+		{
+			get;
+		}
+
+		string ShortEndDateFormat
+		{
+			get;
+		}
+
+		string StartDateField
+		{
+			get;
+		}
+
+		string UnknownText
+		{
+			get;
+		}
+
+		string LocationUnknownField
+		{
+			get;
+		}
+
+		string RegistrationNeededField
+		{
+			get;
+		}
+	}
+}
