f8fe087d6827ffd5700f98ad4a01c257637e042a
agross@therightstuff.de <agross@therightstuff.de@170fb600-b34b-0410-ab5d-8154dd4c2d08>
2008-12-28 16:22:54 +0000

    Layout the groundwork for IoC integration

---
+++ trunk/source/app/DnugLeipzig.Runtime/IoCModule.cs
@@ -0,0 +1,68 @@
+using System.Runtime.CompilerServices;
+using System.Web;
+
+using Castle.Windsor;
+
+namespace DnugLeipzig.Runtime
+{
+	public class IoCModule : IHttpModule
+	{
+		IWindsorContainer _container;
+
+		#region Implementation of IHttpModule
+		/// <summary>
+		/// Initializes a module and prepares it to handle requests.
+		/// </summary>
+		/// <param name="context">An <see cref="T:System.Web.HttpApplication"></see> that provides access to the methods,
+		/// properties, and events common to all application objects within an ASP.NET application </param>
+		public void Init(HttpApplication context)
+		{
+			if (!IoC.IsInitialized)
+			{
+				InitializeContainer(this);
+			}
+		}
+
+		/// <summary>
+		/// Disposes of the resources (other than memory) used by the module that implements <see cref="T:System.Web.IHttpModule">
+		/// </see>.
+		/// </summary>
+		public void Dispose()
+		{
+			if (_container != null)
+			{
+				// Release or cleanup managed resources.
+				IoC.Reset(_container);
+				_container.Dispose();
+			}
+		}
+		#endregion
+
+		[MethodImpl(MethodImplOptions.Synchronized)]
+		void InitializeContainer(IoCModule self)
+		{
+			if (IoC.IsInitialized)
+			{
+				return;
+			}
+			_container = self.InitializeContainer();
+		}
+
+		IWindsorContainer InitializeContainer()
+		{
+			IoC.Initialize(CreateContainerWithMappings());
+			return IoC.Container;
+		}
+
+		protected virtual IWindsorContainer CreateContainerWithMappings()
+		{
+			IWindsorContainer container = new WindsorContainer();
+			foreach (var registration in Registrations.Get())
+			{
+				container.Register(registration);
+			}
+
+			return container;
+		}
+	}
+}
