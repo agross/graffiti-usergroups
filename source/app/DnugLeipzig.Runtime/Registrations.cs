c15a01bafc1b50520420f48a476db4c9b83f74d4
agross <agross@therightstuff.de>
2008-12-28 16:22:54 +0000

    Layout the groundwork for IoC integration

---
+++ source/app/DnugLeipzig.Runtime/Registrations.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Net.Mail;
+
+using Castle.MicroKernel.Registration;
+
+using DnugLeipzig.Definitions;
+
+using Graffiti.Core;
+
+namespace DnugLeipzig.Runtime
+{
+	internal static class Registrations
+	{
+		internal static IEnumerable<IRegistration> Get()
+		{
+			yield break;
+		}
+	}
+}
